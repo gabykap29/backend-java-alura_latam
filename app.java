53141ec899e92cdf1d77c71fbbd28777b7034c2c
Gabriel Acosta <gabriacos@gmail.com>
2024-04-04 16:22:57 -0300

    1er Modulo finalizado

---
+++ app.java
@@ -0,0 +1,11 @@
+class HolaMundo{
+    public static void main(String[]args){
+        System.out.println("Hola mundo!");
+    }
+}
+
+class Saludar{
+    public static void main(String[]args){
+        System.out.println("Hola desde java");
+    };
+};
